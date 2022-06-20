eca0715d2fd9b64ba6aeb6fca9ac795eb72b0efe
jytrack64 <103479942+jytrack64@users.noreply.github.com>
2022-06-20 13:14:01 +0900

    14681: 사분면 고르기
    점이 어느 사분면에 있는지 알아내는 문제

---
+++ 2. 조건문/14681.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input = fs.readFileSync(0).toString().split('\n');
+
+let x = parseInt(input[0]);
+let y = parseInt(input[1]);
+
+if (x > 0 && y > 0) console.log(1)
+else if (x < 0 && y > 0) console.log(2)
+else if (x < 0 && y < 0) console.log(3)
+else console.log(4)
